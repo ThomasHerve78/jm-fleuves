179de00f127bcadb7ce640d9302139debf93f83e
Thomas HERVE <herve.thom78@yahoo.fr>
2020-06-08 22:26:02 +0200

    all stand + order + other stuff

--- js/test.js
+++ js/test.js
@@ -1,9 +1,10 @@
 var displayed = false;
+var aboutDisplayed=false;
 var currentElem= "";
 function init() {
 
 	var listElem = [];
-	for (let index = 1; index < 19; index++) {
+	for (let index = 1; index < 17; index++) {
 		
 		listElem.push(document.getElementById("rect"+index));
 	}
@@ -17,16 +18,25 @@ function init() {
 }
 
 function displayElem(rectId){
+	
 	document.getElementById("base").classList.add('hidden');
-	
+	document.getElementById('about').classList.add('hidden');
+	aboutDisplayed=false;
 
-	
-	if(rectId == currentElem && displayed == true){
+	if (currentElem==""){
+		document.getElementById(rectId).style.fill= "blue";
+		document.getElementById("info-"+rectId).classList.remove('hidden');
+		document.getElementById("base").classList.add('hidden');
+		displayed = true;
+		console.log("hello")
+	}
+	else if(rectId == currentElem && displayed == true){
 		document.getElementById(currentElem).style.fill= "black";
 		document.getElementById("info-"+ currentElem).classList.add('hidden');
 		document.getElementById("base").classList.remove('hidden');
 		displayed = false;
 	}	else if(rectId == currentElem && displayed == false){
+		
 		document.getElementById(currentElem).style.fill= "blue";
 		document.getElementById("info-"+ currentElem).classList.remove('hidden');
 		document.getElementById("base").classList.add('hidden');
@@ -39,14 +49,31 @@ function displayElem(rectId){
 		document.getElementById("info-"+ currentElem).classList.add('hidden');
 		document.getElementById("base").classList.add('hidden');
 		displayed = true;
-	}   else if (currentElem=""){
-		document.getElementById(rectId).style.fill= "blue";
-		document.getElementById("info-"+rectId).classList.remove('hidden');
-		document.getElementById("base").classList.remove('hidden');
-		displayed = true;
+	}  
 
-	}	
+		
 
 	currentElem = rectId;
 	console.log(currentElem)
+}
+function about(){
+	var nous = document.getElementById('about')
+	if (aboutDisplayed==false){
+		document.getElementById("base").classList.add('hidden');
+		nous.classList.remove('hidden');
+		
+		aboutDisplayed=true;
+
+	}
+	else if (aboutDisplayed==true){
+		document.getElementById("base").classList.remove('hidden');
+		nous.classList.add('hidden');
+		aboutDisplayed=false;
+	} else if (aboutDisplayed==false && currentElem!=""){
+		document.getElementById("base").classList.add('hidden');
+		nous.classList.remove('hidden');
+		aboutDisplayed=true;
+		currentElem.classList.add('hidden')
+	}
+		
 }
